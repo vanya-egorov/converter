f57d119222e3f7200f6c2596b31f5f553ac0b7ad
vanya-egorov <vorotavana@gmail.com>
2024-03-09 15:42:01 +0300

    last commit

--- internal/models/employee.go
+++ internal/models/employee.go
@@ -12,14 +12,11 @@ type Employee struct {
     Birthday *string
 }
 
-// Validate проводит проверку валидности полей Employee
 func (e *Employee) Validate() error {
-    // Проверка наличия имени
     if e.Name == "" {
         return errors.New("Имя не может быть пустым")
     }
 
-    // Проверка телефонного номера с помощью регулярного выражения
     phonePattern := ^\+[0-9]{11}$
     matched, _ := regexp.MatchString(phonePattern, e.Phone)
     if !matched {
